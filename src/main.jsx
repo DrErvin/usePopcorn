314a4b83adc57255a902fc66507a8f6b78b252fc
DrErvin <ervin.dragovic@gmail.com>
2025-07-03 20:13:41 +0200

    Finished 121

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,42 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 import StarRating from "./StarRating";
 
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating
+//         maxRating={5}
+//         messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+//         onSetRating={setMovieRating}
+//       />
+//       <p>The movie has {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating />
+
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+
+    <StarRating
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={2}
+    />
+
+    {/* <Test /> */}
+
+    {/* <StarRating /> */}
   </StrictMode>
 );
